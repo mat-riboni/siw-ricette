a5f6943d8fc46d064ded99643f821e8971f261ef
Mattia Riboni <154544827+mat-riboni@users.noreply.github.com>
2024-06-06 17:11:25 +0200

    first commit

---
+++ src/main/java/it/uniroma3/ricette/SiwRicetteApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.ricette;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiwRicetteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiwRicetteApplication.class, args);
+	}
+
+}
